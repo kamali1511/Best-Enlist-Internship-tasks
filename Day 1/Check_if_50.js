29a1dbbd2869ac386acfe1fe974c6bab4421d5a0
Kamalishree A <53998826+kamali1511@users.noreply.github.com>
2020-11-27 21:09:21 +0530

    Add files via upload
    Day1

---
+++ Day 1/Check_if_50.js
@@ -0,0 +1,8 @@
+function Check(num1, num2)
+{
+    return((num1==50||num2==50)||(num1+num2==50));
+}
+
+Check(50,50)
+Check(50,25)
+Check(27,28)
---
+++ Day 1/string-length.js
@@ -0,0 +1,6 @@
+/* The length property in javascript returns the length of a string*/
+
+var str = "Kamalishree";
+var l = str.length;
+
+console.log(l)

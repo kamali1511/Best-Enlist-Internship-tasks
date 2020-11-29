ecbc62b48dbdc8792d089bc292f0c0ce242968cf
Kamalishree A <53998826+kamali1511@users.noreply.github.com>
2020-11-29 22:27:29 +0530

    Update Check_if_50.js
    edit1

--- Day 1/Check_if_50.js
+++ Day 1/Check_if_50.js
@@ -1,3 +1,5 @@
+/* Write a javascript program to check two given numbers and return true if one of the number is 50 or if their sum is 50 */
+
 function Check(num1, num2)
 {
     return((num1==50||num2==50)||(num1+num2==50));

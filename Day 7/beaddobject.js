d13eb16112420f2e433623baffed2b6ea1acab3b
Kamalishree A <53998826+kamali1511@users.noreply.github.com>
2020-12-04 00:01:46 +0530

    Add files via upload
    Day 7

---
+++ Day 7/beaddobject.js
@@ -0,0 +1,20 @@
+/* Write a javascript prototype method named beaddobject which extends Array prototype, to ass an object with its value to the array of objects */
+
+
+var Input = [
+	{
+		name:"Preethi",
+		age:25,
+	},
+	{
+		name:"Divya",
+		age:26,
+	}
+];
+Array.prototype.beAddObject = function (input){
+Input.forEach((item,i) =>{
+	item.Dept = input;
+})
+return(sampleInput);
+}
+sampleInput.beAddObject("Chemical");

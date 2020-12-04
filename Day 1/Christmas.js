f148039fef93d0b310edb87f55ab486bfbaad4a8
Kamalishree A <53998826+kamali1511@users.noreply.github.com>
2020-12-04 12:53:19 +0530

    Update Christmas.js
    edited

--- Day 1/Christmas.js
+++ Day 1/Christmas.js
@@ -4,8 +4,8 @@ today=new Date();
 var Christmas=new Date(today.getFullYear(), 11, 25);
 if (today.getMonth()==11 && today.getDate()>25) 
 {
-Christmas.setFullYear(cmas.getFullYear()+1); 
+Christmas.setFullYear(Christmas.getFullYear()+1); 
 }  
 var one_day=1000*60*60*24;
-console.log(Math.ceil((cmas.getTime()-today.getTime())/(one_day))+
+console.log(Math.ceil((Christmas.getTime()-today.getTime())/(one_day))+
 " days left until Christmas!");
